ce677c4f47f2ebe7b115dc660ee1f139f4383ce5
HatemRamadan <hatemram96@gmail.com>
2017-03-31 21:25:21 +0200

    implemented set_unavailable_slots and set_available_slots

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var playerController=require('../controllers/playerController');
+var serviceProviderController=require('../controllers/serviceProviderController');
 
 var router = express.Router();
 
