9be1f0196a2bf0a7cb606b3ceab503241633931d
Mohamed Ahmed <mohamed.abdel-ghafar@student.guc.edu.eg>
2017-03-31 17:19:08 +0200

    completed Book Free Hours user story

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,8 +1,8 @@
 let Game = require('../models/Game');
-
-let playerController = {
-    
-    viewgames:function(req, res){
+let Booking = require('../models/Booking');
+var moment = require('moment-timezone');
+var SpController  = require('./serviceProviderController');
+var viewgames = function(req, res){
         
         Game.find(function(err, games){
             
@@ -11,7 +11,55 @@ let playerController = {
             else
                 res.render('index', {games});
         })
+    };
+
+var bookHours=  function(month,day,startIndex,endIndex,timestamp,arenaID,playerID,callback){
+   //TODO:handle Last three weeks of the year 
+   //create Booking
+   var indicies = SpController.getScheduleIndicies(month,day);
+   if(checkAvailable(indicies.weekIndex,indicies.dayIndex))
+  {
+    var newBooking = new Booking({
+        player:playerID,
+        arena:arenaID,
+        time_stamp:timestamp,
+        bookDay:day,
+        bookWeek:week,
+        bookMonth:month,
+        accepted:autoAccept
+    }).save(function(err,bookingObj){
+    if(err){
+        callback(err);
     }
+    else
+    {
+ SpController.handleBooking(bookingObj._id);
+    }
+    });
+  }
+  else
+  {
+      callback("Time Unavailable");
+  }
+}
+
+function checkAvailable(weekIndex,dayIndex,startIndex,endIndex,schedule){
+    var pointer = 0;
+    for(pointer=startIndex;pointer<=endIndex;pointer++){
+        if(schedule[weekIndex][dayIndex][pointer]!=0)
+            return false;
+    }
+    return true;
+
+
+}
+
+
+
+let playerController = {
+    
+    viewgames:viewgames,
+    bookHours:bookHours
 }
 
 module.exports = playerController;
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var playerController=require('../controllers/playerController');
-
+var Player = require('../models/Player')
 var router = express.Router();
 
+
+
+
+
+
+
+//book free hours
+router.post('/arena/:arenaId?bookHours',function(req,res){
+   
+Player.findOne({username:req.cookies.username},function(err,player){
+      
+playerController.bookHours(req.body.month,req.body.day,req.body.startIndex,req.body.endIndex,new Date(),req.params.arenaId,player._id,function(err){
+    if(err)
+    {
+        console.log(err);
+        res.send(err);
+    }
+    else
+    {
+        res.redirect('/arena/:arenaId');
+    }
+}) 
+
+ })
+  
+   
+})
 
 
 module.exports = router;
