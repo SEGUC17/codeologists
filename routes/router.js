245e0a73be5e61e03948661a7b65ea6d2e1c6d11
Mostafa-Elmenbawy <elmenabawym@gmail.com>
2017-04-02 06:59:32 +0100

    Black/White lists

--- routes/router.js
+++ routes/router.js
@@ -1,18 +1,30 @@
 var express = require('express');
-var playerController=require('../controllers/playerController');
-var serviceProviderController=require('../controllers/serviceProviderController');
+var playerController = require('../controllers/playerController');
+var serviceProviderController = require('../controllers/serviceProviderController');
 var router = express.Router();
 
 
-router.get('/viewgames',playerController.viewgames);
+router.get('/viewgames', playerController.viewgames);
 
-router.get('/editarena/:arenaname',serviceProviderController.editarena);
+router.get('/editarena/:arenaname', serviceProviderController.editarena);
 
-router.post('/editarenainfo/:arenaname',serviceProviderController.editarenainfo);
+router.post('/editarenainfo/:arenaname', serviceProviderController.editarenainfo);
 
-router.get('/editdefaultschedule/:arenaname',serviceProviderController.editdefaultschedule);
+router.get('/editdefaultschedule/:arenaname', serviceProviderController.editdefaultschedule);
 
-router.post('/addarenaimage/:arenaname',upload.any(),serviceProviderController.addimage);
+router.post('/addarenaimage/:arenaname', upload.any(), serviceProviderController.addimage);
+
+router.post('/profile/blacklist', serviceProviderController.add_to_blacklist);
+
+router.post('/profile/blacklist/phone', serviceProviderController.add_to_blacklist_phone);
+
+router.get('/profile/removeblacklist/:username', serviceProviderController.remove_from_blacklist);
+
+router.post('/profile/whitelist', serviceProviderController.add_to_whitelist);
+
+router.post('/profile/whitelist/phone', serviceProviderController.add_to_whitelist_phone);
+
+router.get('/profile/removewhitelist/:username', serviceProviderController.remove_from_whitelist);
 
 
 module.exports = router;
