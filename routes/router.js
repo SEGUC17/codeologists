e9296431001de09dfe715c59a4b709dc5e92d75e
omar kharouba <omarkharouba4@gmail.com>
2017-04-02 15:35:45 +0100

    edit_arena added

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var playerController=require('../controllers/playerController');
-
+var serviceProviderController=require('../controllers/serviceProviderController');
 var router = express.Router();
 
 
+router.get('/viewgames',playerController.viewgames);
+
+router.get('/editarena/:arenaname',serviceProviderController.editarena);
+
+router.post('/editarenainfo/:arenaname',serviceProviderController.editarenainfo);
+
+router.get('/editdefaultschedule/:arenaname',serviceProviderController.editdefaultschedule);
+
+router.post('/addarenaimage/:arenaname',upload.any(),serviceProviderController.addimage);
 
 module.exports = router;
