c8103ac164111df03f0219e78c48596034151333
AhmedTawfek <ahmed.tawfik96@gmail.com>
2017-04-03 04:44:08 +0200

    Comment and first average but with no testing

--- models/Arena.js
+++ models/Arena.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 var ServiceProvider=require('./ServiceProvider');
-//The schedule is represented as an array of weeks 
-//each of week is and array of days and each day is an array of half hours (i.e: 48 elements each represents a distinct half hour for example the fifth element reprents the half our form 2:30 AM to 3:00 AM)
+//The schedule is represented as an array of weeks
+//each of week is an array of days and each day is an array of half hours (i.e: 48 elements each represents a distinct half hour for example the fifth element reprents the half our form 2:30 AM to 3:00 AM)
 //note that if a certain half hour is free it will contain the value ZERO if it is unavailable due to maintance or some other reason it will be Negative one otherwise if it is booked it will conatin the ID of the booking !
-//If you do not understand please ask any member of the team responsible namely (Omar,Abdel-ghafar,Hatem,Mina,Abdel-zaher,Eladdad,Elmenebawy) 
+//If you do not understand please ask any member of the team responsible namely (Omar,Abdel-ghafar,Hatem,Mina,Abdel-zaher,Eladdad,Elmenebawy)
 var schema = mongoose.Schema({
 	service_provider: {
 		type:mongoose.Schema.Types.ObjectId,
@@ -36,7 +36,7 @@ var schema = mongoose.Schema({
 		type: Number,
 		required: true
 	},
-	comments: [String],
+	comments: [{Content: String, time_stamp: Date, player: String}],
 	ratings_count: Number
 });
 
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,46 @@
 var express = require('express');
+var passport = require("passport");
 var playerController=require('../controllers/playerController');
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.errors = req.flash("error");
+  res.locals.infos = req.flash("info");
+  next();
+});
+
+router.post('/comment', ensureAuthenticated, function(req, res){
+  var player = req.body.username;
+  var content = req.body.comment;
+  req.arena.comments.push({Content: content, time_stamp: new Date(), player: player});
+  res.redirect('/');
+});
+
+router.post('/ProviderRatesBooking', ensureAuthenticated, function(req, res){
+  var player = req.body.booking.player;
+  var rating = req.body.rating;
+  req.booking.player.avg_rating += rating;
+  req.booking.player.avg_rating ++;
+
+});
+
+// router.post('/comment', ensureAuthenticated, function(req, res){
+//   var player = req.body.username;
+//   var content = req.body.comment;
+//   req.arena.comments.push({Content: content, time_stamp: time_stamp, player: player});
+// }
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.flash("error", "You must be logged in to see this page.");
+    //res.redirect("/login");
+  }
+}
 
 
 module.exports = router;
