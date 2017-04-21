f6eaae22f094ddc00b52846f76fa73e134dc39f7
hossamfawzy96 <hossam.fawzy96@gmail.com>
2017-04-21 15:05:17 +0200

    Update serviceProviderController.js

--- controllers/serviceProviderController.js
+++ controllers/serviceProviderController.js
@@ -31,23 +31,21 @@ let serviceProviderController =
             })
         },
         edit_profile_info: function (req, res) { //accepting new info and update the DB record
-            ServiceProvider.findOne({ username: req.user.username }, function (err, result) {
+            ServiceProvider.findOne({ username: req.user.username}, function (err, result) {
                 if (err)
                     res.status(500).json({error: err.message});
                 else {
-                    if (!req.body.name) {
-                        res.status(422).json( { error: "name field is empty!...enter new name", result });
-                        return;
-                    } if (!req.body.email) {
-                        res.status(422).json( { error: "email field is empty!...enter new email", result });
-                        return;
-                    } if (!req.body.phone_number) {
-                        res.status(422).json( { error: "phone number field is empty!...enter new phone number", result });
-                        return;
-                    } if (!req.body.old_password) {
-                        res.status(422).json( { error: "your password is required to confirm changes", result });
-                        return;
-                    }
+                  req.checkBody('name', 'Name is required.').notEmpty();
+                    req.checkBody('old_password', 'Password is required.').notEmpty();
+                    req.checkBody('email', 'Email wrong format').isEmail();
+                    req.checkBody('email', 'Email is required.').notEmpty();
+                    req.checkBody('phone_number', 'Phone number is required.').notEmpty();
+                    req.checkBody('phone_number','not a number.').isNumeric();
+                    var errors = req.validationErrors();
+
+                  if(errors)
+                  return res.status(400).json({errors,result});
+
 
                     hasher(req.body.old_password).verifyAgainst(result.password, function (err, verified) {
                         if (err) {
@@ -56,17 +54,13 @@ let serviceProviderController =
                         }
                         else {
                             if (!verified) {
-                                res.status(422).json({ error: " wrong password" });
+                                res.status(422).json({errors:[{param : 'old_password',msg:'wrong password !'}],result });
                                 return;
                             } else {
                                 result.name = req.body.name;
                                 if (req.body.new_password) {
                                     hasher(req.body.new_password).hash(function (err, hash) {
                                         result.password = hash;
-                                        if (!validateEmail(req.body.email)) {
-                                            res.status(422).json({error:"wrong email format"});
-                                            return;
-                                        }
                                         result.email = req.body.email;
                                         result.phone_number = req.body.phone_number;
                                         if (req.files[0]) {
@@ -89,10 +83,6 @@ let serviceProviderController =
                                     });
                                 }
                                 else {
-                                    if (!validateEmail(req.body.email)) {
-                                        res.status(422).json({error:"wrong email format"});
-                                        return;
-                                    }
                                     result.email = req.body.email;
                                     result.phone_number = req.body.phone_number;
                                     if (req.files[0]) {
