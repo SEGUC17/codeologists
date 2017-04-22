871f679c96c8f7ec377d5b2e8cf9f43c512d53b6
hossamfawzy96 <hossam.fawzy96@gmail.com>
2017-04-22 19:16:55 +0200

    Update playerController.js

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -17,7 +17,25 @@ function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
+function compute(req, res, result) {
+	if (result.length == 0) {
+		res.status(404).json({error:"no Arena matches your value"});
+		return;
+	} else {
+		var tmp = 0;
+		if (!req.body.index)  // in case the page diplayed for the first time not as a result of selecting page index; index will not exit in the body
+			tmp = 1;                                                                                                     // so we set it to one
+		else
+			tmp = req.body.index;
+		var count = Math.ceil(result.length / 10);
+		var start = (tmp - 1) * 10;
+		var end = tmp == count ? result.length : tmp * 10;
+		var active = tmp;  //to indicate the active page rightnow
 
+		res.json({result,count,start,end,active});
+		return;
+	}
+}
 
 let playerController = {
   edit_profile_page: function (req, res) { // prepar the edit profile page
