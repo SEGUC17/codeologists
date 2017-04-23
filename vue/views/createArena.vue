9c604aaa2725777a1633ec19d38f91113749cf23
abdelzaher <aabdelzaher96@gmail.com>
2017-04-23 12:18:58 +0000

    updating front end for cancel booking and create arena

--- vue/views/createArena.vue
+++ vue/views/createArena.vue
@@ -1,63 +1,199 @@
 <template>
 <div>
-  <h1>Create a new arena</h1>
-  <form action = "/createArena" method="post" @submit.prevent = "create" >
-      <input type="text" placeholder="Arena name" v-model = "form.name" required="true"> <br>
-      <input type="text" placeholder="Arena address" v-model = "form.address" required="true"> <br>
-      <input type="text" placeholder="Arena size" v-model = "form.size" required="true"> <br>
-      <input type="text" placeholder="Arena location" v-model = "form.location" required="true"> <br>
-      <input type="text" placeholder="Arena type" v-model = "form.type" required="true"> <br>
-      <input type="text" placeholder="Arena price" v-model = "form.price" required="true"> <br>
-      <input type="text" placeholder="Rules and regulations" v-model = "form.rules_and_regulations"> <br>
 
-      <!--Start of the schedule part-->
-      <h5>{{days[currentDay]}}</h5>
-      <div class="w3-dropdown-hover">
-        <button class="w3-button w3-black">Choose Day</button>
-        <div class="w3-dropdown-content w3-bar-block w3-border">
-          <a v-for="i in 7" class="w3-bar-item w3-button" @click="selectDay(i-1)">{{days[i-1]}}</a>
+
+  <div class="w3-quarter">
+    &nbsp
+  </div>
+
+<div class = "w3-half" style = "width: 900px">
+  <br>
+  <h1>Create a new arena</h1>
+  <el-steps :space="100" :active="active" finish-status="success">
+    <el-step title="Step 1"></el-step>
+    <el-step title="Step 2"></el-step>
+  </el-steps>
+  <form action = "/createArena" method="post" @submit.prevent = "create">
+
+      <!-- Start of info part -->
+      <div style = "width: 720px" name = "info" id = "info" v-if = "this.active == 0">
+        <div class="field">
+          <br><br>
+        <label class="label">Arena Name:</label>
+        <p class="control">
+          <input class="input "type="text" placeholder="Arena name" v-model = "form.name" required="true"> <br>
+        </p>
+        </div>
+
+        <div class="field">
+        <label class="label">Arena address:</label>
+        <p class="control">
+          <input class="input"  type="text" placeholder="Arena address" v-model = "form.address" required="true"> <br>
+        </p>
+        </div>
+
+        <div class="field">
+        <label class="label">Arena size:</label>
+        <p class="control">
+          <input class="input"  type="text" placeholder="Arena size" v-model = "form.size" required="true" onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode < 10 '> <br>
+        </p>
+        </div>
+
+        <div class="field">
+        <label class="label">Arena location:</label>
+        <p class="control">
+          <input class="input" type="text" placeholder="Arena location" v-model = "form.location" required="true"> <br>
+        </p>
+        </div>
+
+        <div class="field">
+        <label class="label">Arena type:</label>
+        <p class="control">
+          <input class="input"  type="text" placeholder="Arena type" v-model = "form.type" required="true"> <br>
+        </p>
+        </div>
+
+        <div class="field">
+        <label class="label">Arena price:</label>
+        <p class="control">
+          <input class="input"  type="text" placeholder="Arena price" v-model = "form.price" required="true" onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode < 10 '> <br>
+        </p>
+        </div>
+
+        <div class="field">
+        <label class="label">Rules and regulations:</label>
+        <p class="control">
+        <textarea class="textarea" placeholder="Rules and regulations" v-model = "form.rules_and_regulations"></textarea><br></p>
         </div>
       </div>
+      <!-- End of info part -->
+
+      <!--Start of the schedule part-->
+      <div name = "schedule" id = "shcedule" v-if = "this.active == 1" style="width: 900px;">
+        <br>
+        <h3>Adjust your default weekly schedule</h3>
+        <!-- <h5>{{days[currentDay]}}</h5>
+        <div class="w3-dropdown-hover">
+              <button class="w3-button w3-black" @click.prevent>Choose a day</button>
+            </tr>
+          </table>
+          <div class="w3-dropdown-content w3-bar-block w3-border">
+            <a v-for="i in 7" class="w3-bar-item w3-button" @click="selectDay(i-1)">{{days[i-1]}}</a>
+          </div>
+        </div> -->
+
+        <div class="tabs ">
+    		  <ul>
+    		    <li :class="{'black-tab' : selectedTab=='sat'}">
+              <a @click="selectedTab='sat'; currentDay = 0">
+                <h6 :class="{'white-text': selectedTab == 'sat'}">Saturday</h6>
+              </a>
+            </li>
+    		    <li :class="{'black-tab' : selectedTab=='sun'}">
+              <a @click="selectedTab='sun'; currentDay = 1">
+                <h6 :class="{'white-text': selectedTab == 'sun'}">Sunday</h6>
+              </a>
+            </li>
+    		    <li :class="{'black-tab' : selectedTab=='mon'}">
+              <a @click="selectedTab='mon'; currentDay = 2">
+                <h6 :class="{'white-text': selectedTab == 'mon'}">Monday</h6>
+              </a>
+            </li>
+            <li :class="{'black-tab' : selectedTab=='tues'}">
+              <a @click="selectedTab='tues'; currentDay = 3">
+                <h6 :class="{'white-text': selectedTab == 'tues'}">Tuesday</h6>
+              </a>
+            </li>
+    		    <li :class="{'black-tab' : selectedTab=='wed'}">
+              <a @click="selectedTab='wed'; currentDay = 4">
+                <h6 :class="{'white-text': selectedTab == 'wed'}">Wednesday</h6>
+              </a>
+            </li>
+    		    <li :class="{'black-tab' : selectedTab=='thurs'}">
+              <a @click="selectedTab='thurs'; currentDay = 5">
+                <h6 :class="{'white-text': selectedTab == 'thurs'}">Thursday</h6>
+              </a>
+            </li>
+            <li :class="{'black-tab' : selectedTab=='fri'}">
+              <a @click="selectedTab='fri'; currentDay = 6">
+                <h6 :class="{'white-text': selectedTab == 'fri'}">Friday</h6>
+              </a>
+            </li>
+    		  </ul>
+    		</div>
 
 
-      <table v-for="j in 7" v-if="j-1==currentDay" class="table is-bordered">
-        <tr>
-          <td v-for="i in 12" :class="{ 'w3-pale-red': schedule[j-1][i-1]==-1, 'w3-pale-green': schedule[j-1][i-1]==0 }">
-            <p>{{slots[i-1]}}</p>
-            <input type="checkbox" name="daySlot" @click="toggleSlot(j-1,i-1)" :checked="schedule[j-1][i-1]==-1">
-          </td>
-        </tr>
+        <table v-for="j in 7" v-if="j-1==currentDay" class="table is-bordered">
+          <tr>
+            <td v-for="i in 12" >
+              <div @click="toggleSlot(j-1,i-1)">
+                <a >
+                  <p>{{slots[i-1]}}</p>
+                  <img v-if = "schedule[j-1][i-1] == -1" src="cross.png">
+                  <img v-if = "schedule[j-1][i-1] == 0" src="tick.png">
+                </a>
+            </div>
+            </td>
+          </tr>
 
-        <tr>
-          <td v-for="i in 12" :class="{ 'w3-pale-red': schedule[j-1][i+11]==-1, 'w3-pale-green': schedule[j-1][i+11]==0 }">
-            <p>{{slots[11+i]}}</p>
-            <input type="checkbox" name="daySlot" @click="toggleSlot(j-1,i+11)" :checked="schedule[j-1][11+i]==-1">
-          </td>
-        </tr>
+          <tr>
+            <td v-for="i in 12">
+              <div @click="toggleSlot(j-1,i+11)">
+                <a >
+                  <p>{{slots[i+11]}}</p>
+                  <img v-if = "schedule[j-1][i+11] == -1" src="cross.png">
+                  <img v-if = "schedule[j-1][i+11] == 0" src="tick.png">
+                </a>
+              </div>
+            </td>
+          </tr>
 
-        <tr>
-          <td v-for="i in 12" :class="{ 'w3-pale-red': schedule[j-1][23+i]==-1, 'w3-pale-green': schedule[j-1][23+i]==0 }">
-            <p>{{slots[23+i]}}</p>
-            <input type="checkbox" name="daySlot" @click="toggleSlot(j-1,i+23)" :checked="schedule[j-1][23+i]==-1">
-          </td>
-        </tr>
+          <tr>
+            <td v-for="i in 12">
+              <div @click="toggleSlot(j-1, i+23)">
+                <a >
+                  <p>{{slots[i+23]}}</p>
+                  <img v-if = "schedule[j-1][i+23] == -1" src="cross.png">
+                  <img v-if = "schedule[j-1][i+23] == 0" src="tick.png">
+                </a>
+              </div>
+            </td>
+          </tr>
 
-        <tr>
-          <td v-for="i in 12" :class="{ 'w3-pale-red': schedule[j-1][35+i]==-1, 'w3-pale-green': schedule[j-1][35+i]==0 }">
-            <p>{{slots[35+i]}}</p>
-            <input type="checkbox" name="daySlot" @click="toggleSlot(j-1,i+35)" :checked="schedule[j-1][35+i]==-1">
-          </td>
-        </tr>
-      </table>
-
-
+          <tr>
+            <td v-for="i in 12" >
+              <div @click = "toggleSlot(j-1, i+35)">
+                <a >
+                  <p>{{slots[i+35]}}</p>
+                  <img v-if = "schedule[j-1][i+35] == -1" src="cross.png">
+                  <img v-if = "schedule[j-1][i+35] == 0" src="tick.png">
+                </a>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
       <!--End of the schedule part-->
 
-      <div class="control">
-        <button class="button is-primary">Add Arena</button>
-      </div>
+      <el-button class = "button is-primary" :disabled="form.name === ''
+      || form.address === '' || form.location === '' || form.size === '' || form.type === '' || form.price === ''"
+       v-if = "active == 0" @click="next">Next</el-button>
+      <table>
+        <tr>
+          <td><el-button class = "button is-primary" @click="next" v-if = "active == 1">Back</el-button>
+          </td>
+          <td><div class="control" v-if = "active == 1">
+            <button class="button is-primary">Add Arena</button>
+          </div></td>
+        </tr>
+      </table>
   </form>
 
+<br>
+</div>
+<div class="w3-quarter">
+&nbsp
+</div>
 </div>
 </template>
 
@@ -84,7 +220,9 @@
         schedule: [],
         slots: [],
         days: [],
-        currentDay: 0
+        currentDay: 0,
+        active: 0,
+        selectedTab : 'sat'
       }
     },
 
@@ -131,7 +269,7 @@
         this.form.tuesday = arr[3];
         this.form.wednesday = arr[4];
         this.form.thursday = arr[5];
-        this.form.fri = arr[6];
+        this.form.friday = arr[6];
 
         this.form.submit('post','/createArena')
 				.then(res => {
@@ -160,7 +298,13 @@
         } else {
           this.schedule[day][slot] = 0;
         }
+        this.currentDay += 1;
+        this.currentDay -= 1;
       },
+
+      next() {
+       this.active = this.active== 0?1:0;
+     }
     }
   }
 </script>
--- vue/views/delete_booking.vue
+++ vue/views/delete_booking.vue
@@ -1,14 +1,94 @@
 <template>
-    <div id = "root">
-      <ul>
-        <li v-for = "booking in bookings">
-          Booking at {{booking.arena}} on {{booking.bookDay}}/{{booking.bookMonth}} from {{hour(booking.start_index)}}:{{minute(booking.start_index)
-          }} to {{hour(booking.end_index)}}:{{minute(booking.end_index)
-          }}
-        <button type="button" @click = 'cancel(booking)'>Delete</button></li>
-      </ul>
+  <div id = "root" style=" margin-left: 2cm;" >
+
+
+    <br>
+    <h2>Your current bookings:</h2>
+    <br><br>
+    <div class="" v-for = "booking in bookings">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span style="line-height: 36px; font-size: 20px; font-weight: bold;">{{booking.arena}}</span>
+          <el-button style="float: right;" type="danger" @click = 'cancel(booking)'>Cancel</el-button>
+        </div>
+        <div>
+          <table>
+            <tr style="font-size: 12px">
+              <td>Date: {{month(booking.bookMonth)}} {{booking.bookDay}}</td>
+              <td>From: {{hour(booking.start_index)}}:{{minute(booking.start_index)}}</td>
+              <td>To: {{hour(booking.end_index)}}:{{minute(booking.end_index)}}</td>
+            </tr>
+          </table>
+        </div>
+      </el-card>
+      <br><br>
+    </div>
+
+
+
+
+<br><br><br><br><br><br>
+
+
+      <div class="tile is-parent" v-for = "booking in bookings">
+          <article class="tile is-child notification">
+            <table>
+              <tr>
+                <td>{{booking.arena}}</td>
+                <td>{{booking.bookDay}}/{{booking.bookMonth}}</td>
+                <td>{{hour(booking.start_index)}}:{{minute(booking.start_index)
+                }}</td>
+                <td>{{hour(booking.end_index)}}:{{minute(booking.end_index)
+                }}</td>
+                <!-- <td><button type="button" @click = 'cancel(booking)' class="button is-danger">Delete</button></td> -->
+                <td><a class="delete button is-danger" @click = 'cancel(booking)' ></a></td>
+              </tr>
+            </table>
+          </article>
+        </div>
+
+
+
+        <br><br><br><br><br><br>
+
+
+
+      <br>
+      <h2>Your current bookings</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Arena</th>
+            <th>Date</th>
+            <th>From</th>
+            <th>To</th>
+            <th>&nbsp</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+            <td>&nbsp</td>
+
+          </tr>
+          <tr v-for = "booking in bookings">
+            <td>{{booking.arena}}</td>
+            <td>{{booking.bookDay}}/{{booking.bookMonth}}</td>
+            <td>{{hour(booking.start_index)}}:{{minute(booking.start_index)
+            }}</td>
+            <td>{{hour(booking.end_index)}}:{{minute(booking.end_index)
+            }}</td>
+            <!-- <td><button type="button" @click = 'cancel(booking)' class="button is-danger">Delete</button></td> -->
+            <td><a class="delete button is-danger" @click = 'cancel(booking)' ></a></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 </template>
+
 
 <script>
   export default({
@@ -33,6 +113,25 @@
 
       minute(index){
         return (index%2) == 0?'00':'30';
+      },
+
+      month(i)
+      {
+        switch(i){
+          case 1: return 'January';
+          case 2: return 'February';
+          case 3: return 'March';
+          case 4: return 'April';
+          case 5: return 'May';
+          case 6: return 'June';
+          case 7: return 'July';
+          case 8:  return 'August';
+          case 9: return 'September';
+          case 10: return 'October';
+          case 11: return 'November';
+          case 12: return 'December';
+          default: return '';
+        }
       },
 
       cancel(booking){
